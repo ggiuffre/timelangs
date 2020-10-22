bef65a7298fe293b5325003af90cace3f50ebf5b
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-10-22 09:54:25 +0200

    Convert TimelineItem to function component

--- src/TimelineItem.js
+++ src/TimelineItem.js
@@ -1,41 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Timeline.css';
 import './TimelineItem.css';
 
-class TimelineItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {detailsVisible: false};
-    this.tags = <ul className="tagsList">{
-      this.props.tags.map(tag => <li key={tag}>{tag}</li>)
+function TimelineItem(props) {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const showDetails = () => { // TODO
+    setDetailsVisible(!detailsVisible);
+  };
+
+  const tags = <ul className="tagsList">{
+      props.tags.map(tag => <li key={tag}>{tag}</li>)
     }</ul>;
 
-    this.showDetails = this.showDetails.bind(this);
-  }
+  const classes = [
+    'TimelineItem',
+    props.type,
+    props.side,
+    detailsVisible ? 'detailed' : ''
+  ].join(' ');
 
-  showDetails() { // TODO
-    this.setState(state => ({
-      detailsVisible: !state.detailsVisible
-    }));
-  }
-
-  render() {
-    const classes = [
-      'TimelineItem',
-      this.props.type,
-      this.props.side,
-      this.state.detailsVisible ? 'detailed' : ''
-    ].join(' ');
-    return <li
-      className={classes}
-      onClick={this.showDetails}
-      >
-      <span className="content">
-        {this.props.name} ({this.props.birth})
-        {this.tags}
-      </span>
-    </li>;
-  }
+  return <li
+    className={classes}
+    onClick={showDetails}
+    >
+    <span className="content">
+      {props.name} ({props.birth})
+      {tags}
+    </span>
+  </li>;
 }
 
 export default TimelineItem;
