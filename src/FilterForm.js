ef5c8b0e4d5c008580546a8adc91ffedbe69e51b
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-10-22 13:01:17 +0200

    Convert FilterForm to function component

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -1,50 +1,38 @@
 import React from 'react';
 import './FilterForm.css';
 
-function tagsConcat(tags, lang) { return [...tags, ...lang.tags]; }
-function clean(array) { return Array.from(new Set(array)); }
+const tagsConcat = (tags, lang) => [...tags, ...lang.tags];
+const clean = (array) => Array.from(new Set(array));
 
-class FilterForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updateTopic = this.props.updateTopic;
-    this.updateTimeline = this.props.updateTimeline;
-  }
+function FilterForm({ topic, fullPage, updateTopic, updateTimeline }) {
+  const tags = clean(require('./languages.json').reduce(tagsConcat, []));
+  const options = tags
+    .map(tag => <option value={tag} key={tag} />)
+    .sort((a, b) => a.key.localeCompare(b.key));
 
-  componentDidMount() {
-    const tags = clean(require('./languages.json').reduce(tagsConcat, []));
-    this.options = tags
-      .map(tag => <option value={tag} key={tag} />)
-      .sort((a, b) => a.key.localeCompare(b.key));
-  }
+  return <form
+      className={fullPage ? 'fullpage' : ''}
+      name="filter"
+      id="filter"
+      onSubmit={updateTimeline}
+    >
+    <label id="topic">Topic: <input
+        tabIndex="1"
+        type="text"
+        value={topic}
+        onChange={updateTopic}
+        placeholder="OOP, Lisp, JVM..."
+        list="topics"
+      />
+    </label>
+    <datalist id="topics">
+      {options}
+    </datalist>
 
-  render() {
-
-    // the whole form:
-    return <form
-        className={this.props.fullPage ? 'fullpage' : ''}
-        name="filter"
-        id="filter"
-        onSubmit={this.updateTimeline}
-      >
-      <label id="topic">Topic: <input
-          tabIndex="1"
-          type="text"
-          value={this.props.topic}
-          onChange={this.updateTopic}
-          placeholder="OOP, Lisp, JVM..."
-          list="topics"
-        />
-      </label>
-      <datalist id="topics">
-        {this.options}
-      </datalist>
-
-      <label>
-        <input type="submit" value="Update" />
-      </label>
-    </form>;
-  }
+    <label>
+      <input type="submit" value="Update" />
+    </label>
+  </form>;
 }
 
 export default FilterForm;
