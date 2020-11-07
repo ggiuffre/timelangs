a7f2bada059a0604e663d8ec712c3d4d6eb1b1df
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-11-07 11:34:00 +0100

    Simplify state management and other logic of App component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,32 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import FilterForm from './FilterForm.js';
 import Timeline from './Timeline.js';
 
 function App ({ languages, tags }) {
-  const languagesWithTopic = useCallback(topic => {
-    if (topic === '') {
-      return languages;
-    } else {
-      return languages.filter(
-        language => language.tags.includes(topic) 
-      );
-    }
-  }, [languages]);
-
   const { tag } = useParams();
   const history = useHistory();
 
   const [topicFormFullPage, setTopicFormFullPage] = useState(!tag);
-  const [currentTopic, setCurrentTopic] = useState('');
-  const [currentLanguages, setCurrentLanguages] = useState([]);
+  const [currentTopic, setCurrentTopic] = useState(''); // controls FilterForm
 
-  // update state upon change in URL query parameters:
+  // update state and window upon change in URL query parameters:
   useEffect(() => {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
     setCurrentTopic(tag || '');
-    setCurrentLanguages(languagesWithTopic(tag || ''));
-  }, [tag, languagesWithTopic]);
-
-  // scroll to top upon change of topic:
-  useEffect(() => window.scrollTo(0, 0), [currentTopic]);
+  }, [tag]);
 
   const updateTopic = event => setCurrentTopic(event.target.value);
 
   const updateTimeline = event => {
-    // if the timeline is updated due to a browser event...
-    if (arguments.length > 0) {
-      // avoid refreshing the page:
-      event.preventDefault();
+    // avoid refreshing the page:
+    if (event) event.preventDefault();
 
-      // update the browser history with a new URL:
-      history.push(`/${currentTopic}`);
+    // update the browser history with a new URL:
+    history.push(`/${currentTopic}`);
 
-      // minimize the topic form after its first use:
-      if (topicFormFullPage)
-        setTopicFormFullPage(false);
-    }
+    // minimize the topic form after its first use:
+    if (topicFormFullPage) setTopicFormFullPage(false);
   };
 
   const filterForm = <FilterForm
@@ -55,6 +37,9 @@ function App ({ languages, tags }) {
       fullPage={topicFormFullPage}
     />;
 
+  const currentLanguages = tag ? languages.filter(
+      language => language.tags.includes(tag)
+    ) : languages;
   const timeline = topicFormFullPage ? '' :
     <Timeline items={currentLanguages} />;
 
